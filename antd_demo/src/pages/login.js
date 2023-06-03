fcf962dcfce4dd768856552a6da5b786ea0e2d9c
jeanneLxy <123762147+jeanneLxy@users.noreply.github.com>
2023-06-03 21:02:08 +0200

    Update login.js

--- antd_demo/src/pages/login.js
+++ antd_demo/src/pages/login.js
@@ -1,7 +1,75 @@
 import React from "react"
+import { Row, Col, Card, Form, Input, Button, message } from 'antd'
+import { logo } from "../tools"
+import { useNavigate } from 'react-router-dom'
+
+
 const Login = () => {
-  return (
-    <div>Login</div>
+  const navigate = useNavigate()
+
+  return (<Row>
+    <Col
+      md={{
+        span: 8,
+        push: 8
+      }}
+      xs={{
+        span: 22,
+        push: 1
+      }}
+    >
+      <img src={logo}
+        style={{
+          display: "block",
+          margin: '20px auto',
+          borderRadius: '16px',
+          width: '200px',
+        }} alt='login logo' />
+      <Card title='MyCloud'>
+        <Form labelCol={{
+          md: {
+            span: 4,
+          }
+        }}
+          onFinish={(v) => {
+            console.log(v)
+            message.success('Succes login')
+            navigate('/admin/personal_info')
+          }}
+        >
+          <Form.Item
+            label='User name'
+            name='userName'
+            rules={[
+              {
+                required: true,
+                message: 'Please enter username'
+              }
+            ]}
+          >
+            <Input placeholder="Please enter username" />
+          </Form.Item>
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              {
+                required: true,
+                message: 'Please enter password'
+              }
+            ]}>
+            <Input.Password placeholder="Please enter password" />
+          </Form.Item>
+          <Button htmlType="submit" type='primary' style={{
+            display: 'block',
+            margin: '8px auto',
+            width: '10vw',
+          }}> Submit</Button>
+        </Form>
+      </Card>
+
+    </Col>
+  </Row>
   )
 }
 
