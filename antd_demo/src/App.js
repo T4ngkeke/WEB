cd09fd23a28c15c0033dd391e23164953a55f071
T4ngkeke <96550407+T4ngkeke@users.noreply.github.com>
2023-06-11 20:39:22 +0200

    Update App.js

--- antd_demo/src/App.js
+++ antd_demo/src/App.js
@@ -1,20 +1,118 @@
-import MyLayout from "./components/MyLayout"
-import { Routes, Route } from 'react-router-dom'
-import User from "./pages/user"
-import Categories from "./pages/FILE/categories"
-import FileList from "./pages/FILE/list"
+import React, { useState } from "react"
+import { Card, Button, Form, Input, Table, Space, Modal, message } from 'antd'
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons"
+import MyUploader from "../../components/MyUploader"
+import axios from "axios"
 
-const App = () => {
+const Categories = () => {
+  const [isShow, setIsShow] = useState(false)  //controle Uploading Modal present/hide
+  const [myForm] = Form.useForm() //get form element
+  const [filesData,setFilesData]=useState([])
+
+
+  function getData(){
+    axios({
+      method:"GET",
+      url:"/files",
+    })
+    .then((response)=>setFilesData(response.data)).catch((error)=>{
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      }
+    })}
+
+
+    
+    
   return (
-    <MyLayout>
-      <Routes>
-        <Route path='personal_info' element={<User />} />
-        <Route path='upload/file_info' element={<FileList />} />
-        <Route path='upload/file_categories' element={<Categories />} />
+    <>
+      <Card title='File List'
+        extra={<Button type="primary" icon={<PlusOutlined onClick={() => {
+          setIsShow(true)
+        }} />}></Button>}
+      >
+        {getData()}
+        <Space direction="vertical" style={{ width: '100%' }}>
+          <Form layout="inline"
+            onFinish={(v) => {
+              message.success('Search succeed')
+            }}
+          >
+            <Form.Item label='FileName' name='name'>
+              <Input placeholder="Please enter key word" />
+            </Form.Item>
+            <Button
+              htmlType="submit"
+              type='primary'
+              icon={<SearchOutlined />}
+            />
+          </Form>
+          
+          <Table columns={[
+            {
+              title: 'Number',
+              width: 80,
+              align: "center",
+              dataIndex:'number'
+            }, {
+              title: 'Name',
+              dataIndex:'file_name'
+            }, {
+              title: 'Intro',
+              width: 180,
+              dataIndex:'file_extension'
+            }, {
+              title: 'Date',
+              dataIndex:'file_date'
+            }, {
+              title: 'Size',
+              dataIndex:'file_size'
+            }
+          ]} 
+        dataSource={filesData}
+          />
+        </Space>
+      </Card>
+      <Modal
+        title='Uploading'
+        open={isShow}
+        maskClosable={false} //click other layer => not close Modal
+        onCancel={() => setIsShow(false)}
+        onOk={() => {
+          myForm.submit()  //manually trigger form.submit
+        }}
+        destroyOnClose   //clear all when close Modal
+      >
 
-      </Routes>
-    </MyLayout>
+        <Form
+          preserve={false}  //necessary for clear data when close Modal
+          onFinish={(v) => {  //提交表单且数据验证成功后回调事件
+            message.success('success uploading')
+            console.log(v)
+          }}
+          labelCol={{ span: 3 }}
+          form={myForm} //define form to get form content
+        >
+          <Form.Item label='Name' name='name' rules={[{
+            required: true,
+            message: 'Please enter file name'
+          }]}>
+            <Input placeholder="Please enter file name" />
+          </Form.Item>
+          <Form.Item label='File'>
+            <MyUploader />
+          </Form.Item>
+          <Form.Item label='Intro' name='intro'>
+            <Input.TextArea placeholder="Please describe your file" />
+          </Form.Item>
+        </Form>
+
+      </Modal>
+    </>
+
   )
 }
 
-export default App
+export default Categories
