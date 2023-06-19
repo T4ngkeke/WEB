05ec820736f5ef88981492a3d6076be2174a88aa
T4ngkeke <96550407+T4ngkeke@users.noreply.github.com>
2023-06-19 10:48:17 +0200

    Update user.js

--- antd_demo/src/pages/user.js
+++ antd_demo/src/pages/user.js
@@ -1,7 +1,17 @@
 import React from "react"
+import {Card} from 'antd'
 const User = () => {
   return (
-    <div>User</div>
+    <div>   <Card
+    title="Default size card"
+    style={{
+      width: 300,
+    }}
+  >
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card></div>
   )
 }
 
